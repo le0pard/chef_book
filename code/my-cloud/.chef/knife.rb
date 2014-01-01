3edde41603365105b99c155467a7ffee8796ffa2
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-01-01 16:44:32 +0200

    data bags

--- code/my-cloud/.chef/knife.rb
+++ code/my-cloud/.chef/knife.rb
@@ -3,6 +3,6 @@ node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+encrypted_data_bag_secret ".chef/encrypted_data_bag_secret"
 
 knife[:berkshelf_path] = "cookbooks"
