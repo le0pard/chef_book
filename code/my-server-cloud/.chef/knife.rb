201b05a7f4791e5e835541eee0a004f30e9747a8
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-01-05 23:01:57 +0200

    roles

--- code/my-server-cloud/.chef/knife.rb
+++ code/my-server-cloud/.chef/knife.rb
@@ -7,7 +7,7 @@ client_key               "#{current_dir}/leo.pem"
 syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
 validation_client_name   "chef-validator"
 validation_key           "#{current_dir}/chef-validator.pem"
-chef_server_url          "https://chef.server-cloud.dev"
+chef_server_url          "https://10.33.33.33"
 cookbook_path            ["#{current_dir}/../cookbooks", "#{current_dir}/../site-cookbooks"]
 node_path                 "#{current_dir}/../nodes"
 role_path                 "#{current_dir}/../roles"
