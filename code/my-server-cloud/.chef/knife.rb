d2ba2a81f5a6906bd9b6dbb254daee754ad0f4d2
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-01-05 17:44:19 +0200

    server

--- code/my-server-cloud/.chef/knife.rb
+++ code/my-server-cloud/.chef/knife.rb
@@ -1,8 +1,18 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
+current_dir = File.dirname(__FILE__)
+
+log_level                :info
+log_location             STDOUT
+node_name                'leo'
+client_key               "#{current_dir}/leo.pem"
+syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
+validation_client_name   "chef-validator"
+validation_key           "#{current_dir}/chef-validator.pem"
+chef_server_url          "https://chef.server-cloud.dev"
+cookbook_path            ["#{current_dir}/../cookbooks", "#{current_dir}/../site-cookbooks"]
+node_path                 "#{current_dir}/../nodes"
+role_path                 "#{current_dir}/../roles"
+data_bag_path             "#{current_dir}/../data_bags"
+environment_path          "#{current_dir}/../environments"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
