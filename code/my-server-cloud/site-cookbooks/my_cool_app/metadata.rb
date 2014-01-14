af59190117cb41e22bc56bbd508325f69d8cb719
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-01-15 00:03:51 +0200

    begin cookbook

---
+++ code/my-server-cloud/site-cookbooks/my_cool_app/metadata.rb
@@ -0,0 +1,8 @@
+name             'my_cool_app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures my_cool_app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
---
+++ code/my-server-cloud/site-cookbooks/my_cool_app/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: my_cool_app
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
