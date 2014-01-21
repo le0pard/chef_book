caa638ebeab6db0aedbf5410a307aeedc77d16a0
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-01-21 20:38:06 +0200

    new text

--- code/my-server-cloud/site-cookbooks/my_cool_app/recipes/default.rb
+++ code/my-server-cloud/site-cookbooks/my_cool_app/recipes/default.rb
@@ -2,7 +2,11 @@
 # Cookbook Name:: my_cool_app
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
+# Copyright (C) 2014 Alexey Vasiliev
 #
+# MIT
+#
+
+%w(git ntp).each do |pack|
+  package pack
+end
