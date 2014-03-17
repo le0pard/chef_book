1b98eb7546daec236a3ca83c6a27b1479db62ac7
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-03-17 21:07:25 +0200

    minitests + cover

--- code/my-server-cloud/site-cookbooks/my_cool_app/tests/default_test.rb
+++ code/my-server-cloud/site-cookbooks/my_cool_app/tests/default_test.rb
@@ -2,7 +2,7 @@ require 'minitest/spec'
 
 describe_recipe 'my_cool_app::default' do
 
-  %w().ech do |pack|
+  %w(git ntp).ech do |pack|
     it "install #{pack}" do
       package(pack).must_be_installed
     end
