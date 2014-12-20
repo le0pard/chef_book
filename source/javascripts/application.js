b32f5820286f43c716d3c65e31aca149738c48b2
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-12-21 01:33:17 +0200

    cleanup

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require ./vendors/modernizr
-//= require ./vendors/jquery-2.1.1.min
-//= require ./vendors/fastclick
-
-//= require ./vendors/foundation/foundation
+//= require jquery
+//= require fastclick
 
 //= require ./chef
